06136a830f2a6f9f119fb6675019012e3147716f
StoneRicky <739799980@qq.com>
2026-01-28 13:39:17 +0800

    改回自定义的地图

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -231,17 +231,17 @@ export const SWIMMING_COLOR = 'rgb(255,51,51)';
 
 // map tiles vendor, maptiler or mapbox or stadiamaps
 // if you want to use maptiler, set the access token in MAP_TILE_ACCESS_TOKEN
-export const MAP_TILE_VENDOR = 'mapcn';
+export const MAP_TILE_VENDOR = 'maptiler';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
-export const MAP_TILE_STYLE_LIGHT = 'osm-bright';
-export const MAP_TILE_STYLE_DARK = 'dark-matter';
+export const MAP_TILE_STYLE_LIGHT = 'dataviz-light';
+export const MAP_TILE_STYLE_DARK = 'backdrop-dark';
 
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 | sign up at https://client.stadiamaps.com/signup/
 // mapcn: empty
-export const MAP_TILE_ACCESS_TOKEN = '';
+export const MAP_TILE_ACCESS_TOKEN = 'zsyILl2XTNXD1caH8NQf';
 
 export const MAP_TILE_STYLES = {
   mapcn: {
