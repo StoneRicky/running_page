2c724570565c45fb02aa22e646ffb3acd523913e
StoneRicky <739799980@qq.com>
2026-01-28 21:51:38 +0800

    增加脚本内容

--- gen-svg.ps1
+++ gen-svg.ps1
@@ -22,4 +22,13 @@ python run_page/gen_svg.py --from-db --year $CURRENT_YEAR --language zh_CN --tit
 
 python run_page/gen_svg.py --from-db --type year_summary --output assets/year_summary.svg --athlete "$ATHLETE"
 python run_page/save_to_parqent.py
+
+$BIRTHDAY_MONTH = "1995-08"
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol_running.svg --use-localtime --athlete "$ATHLETE" --title "Runner Month of Life" --sport-type running
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol_walking.svg --use-localtime --athlete "$ATHLETE" --title "Walker Month of Life" --sport-type walking
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol_hiking.svg --use-localtime --athlete "$ATHLETE" --title "Hiker Month of Life" --sport-type hiking
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol_cycling.svg --use-localtime --athlete "$ATHLETE" --title "Cyclist Month of Life" --sport-type cycling
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol.svg --use-localtime --athlete "$ATHLETE" --title "Month of Life" --sport-type all
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol_swimming.svg --use-localtime --athlete "$ATHLETE" --title "Swimmer Month of Life" --sport-type swimming
+python run_page/gen_svg.py --from-db --type monthoflife --birth "$BIRTHDAY_MONTH" --output assets/mol_skiing.svg --use-localtime --athlete "$ATHLETE" --title "Skier Month of Life" --sport-type skiing
 Write-Host "success! please check assets directory." -ForegroundColor Green
