92b030f89b169f5b49a6a0c0bf581a3913418110
StoneRicky <739799980@qq.com>
2026-03-06 15:08:44 +0800

    自定义地图主题

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1Ijoic3RvbmVyaWNreSIsImEiOiJjbWtjZjNsZ2owMDVpM2ZwdzZ1b2E5ajd6In0.x7nGC4a2wkN7WoX1fs3MlA';
+  '';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -263,7 +263,9 @@ export const MAP_TILE_STYLES = {
     'dark-matter': 'https://tiles.openfreemap.org/styles/dark',
   },
   maptiler: {
-    'dataviz-light': 'https://api.maptiler.com/maps/dataviz-v4/style.json?key=',
+    // 'dataviz-light': 'https://api.maptiler.com/maps/dataviz-v4/style.json?key=',
+    'dataviz-light':
+      'https://api.maptiler.com/maps/019cc1f2-1c5f-7025-8b79-6cc663ffb443/style.json?key=',
     'dataviz-dark':
       'https://api.maptiler.com/maps/dataviz-dark/style.json?key=',
     'basic-light': 'https://api.maptiler.com/maps/basic-v2/style.json?key=',
