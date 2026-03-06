a9e95529cb2e25b2d0beac845d0fe6ce1b8b3625
StoneRicky <739799980@qq.com>
2026-03-06 15:18:29 +0800

    加入mapbox_token

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  '';
+  'pk.eyJ1Ijoic3RvbmVyaWNreSIsImEiOiJjbWtjZjNsZ2owMDVpM2ZwdzZ1b2E5ajd6In0.x7nGC4a2wkN7WoX1fs3MlA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
